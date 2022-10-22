325a65fea778a727f2967a9f9fdbd09824fc59a4
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-10-22 11:42:54 +0400

    lambda expressions

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return  (list) -> {
+            List<Integer> ls = new ArrayList<>();
+            for (int el : list){
+                ls.add(el / divider);
+            }
+            return ls;
+        };
     }
 }
